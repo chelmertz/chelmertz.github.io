270528bf7a9b654516e20a87b6a0a63c830f11bf
Carl Helmertz <helmertz@gmail.com>
2018-01-07 03:06:46 +0100

    Add deps correctly
    Signed-off-by: Carl Helmertz <helmertz@gmail.com>

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+sh j.sh bundle install
 exec sh j.sh jekyll build -s my_site
--- j.sh
+++ j.sh
@@ -8,6 +8,7 @@ JEKYLL_VERSION=3.5
 
 docker run --rm \
   --volume="$PWD:/srv/jekyll" \
+  --volume="$PWD/vendor/bundle:/usr/local/bundle" \
   -p 4000:4000 \
   -it jekyll/jekyll:$JEKYLL_VERSION \
   $@
