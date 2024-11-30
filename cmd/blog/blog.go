8c408d6f5375052d4fbb93e7e637147d55b89411
Calle Helmertz <helmertz@gmail.com>
2024-11-30 22:54:29 +0100

    cmd/blog: generate whole blog, start with assets

---
+++ cmd/blog/blog.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"io"
+	"os"
+	"path/filepath"
+)
+
+const docs = "docs"
+
+func main() {
+	// github  pages wants to serve either / or docs/, let's go with docs/
+	must(os.RemoveAll(docs))
+	must(os.Mkdir(docs, 0775))
+
+	// docs/assets
+	must(os.Mkdir(filepath.Join(docs, "assets"), 0775))
+	filepath.WalkDir("assets", func(path string, d os.DirEntry, err error) error {
+		if d.IsDir() {
+			must(os.MkdirAll(filepath.Join(docs, path), 0775))
+			return nil
+		}
+		src := must1(os.Open(path))
+		dst := must1(os.Create(filepath.Join(docs, path)))
+		_ = must1(io.Copy(dst, src))
+		return nil
+	})
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func must1[T any](something T, err error) T {
+	must(err)
+	return something
+}
