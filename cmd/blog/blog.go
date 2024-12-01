93ff338c7b2c7897289f4ea96aafd76af7e59ffc
Calle Helmertz <helmertz@gmail.com>
2024-12-01 01:15:02 +0100

    replace some attributes in post.html

--- cmd/blog/blog.go
+++ cmd/blog/blog.go
@@ -49,6 +49,13 @@ func main() {
 	)
 
 	indexHtmlData := IndexHtmlData{}
+	postTemplate := template.Must(template.New("post.html").
+		Funcs(template.FuncMap{
+			"rfc3339": func(t time.Time) string {
+				return t.Format(time.RFC3339)
+			},
+		}).
+		ParseGlob("cmd/blog/post.html"))
 	filepath.WalkDir("_posts", func(path string, d os.DirEntry, err error) error {
 		if path == "_posts" {
 			return nil
@@ -64,11 +71,12 @@ func main() {
 			panic(fmt.Sprintf("no frontmatter found in %s\n", path))
 		}
 
-		var decodedFrontMatter PostMeta
-		must(postsFrontMatter.Decode(&decodedFrontMatter))
+		var post Post
+		must(postsFrontMatter.Decode(&post))
+		post.Content = template.HTML(outputHtml.String())
 
 		var publishedAt time.Time
-		if decodedFrontMatter.Date == "" {
+		if post.Date == "" {
 			// this is stupid, jekyll was bc-safe: date could be in the filename rather than frontmatter
 			// this should be a one-time pass to migrate the old posts.. later?
 			match := dateTimeInFilename.Find([]byte(d.Name()))
@@ -79,38 +87,41 @@ func main() {
 			publishedAt = must1(time.Parse(time.DateTime, fmt.Sprintf("%s-%s-%s %s:%s:00", timeParts[0], timeParts[1], timeParts[2], timeParts[3], timeParts[4])))
 		} else {
 			// the frontmatter's "date" granularity is in minutes
-			publishedAt, err = time.Parse(time.DateTime, decodedFrontMatter.Date+":00")
+			publishedAt, err = time.Parse(time.DateTime, post.Date+":00")
 			if err != nil {
 				panic(fmt.Sprintf("error parsing date from frontmatter for path %s: %v\n", path, err))
 			}
 		}
-		decodedFrontMatter.PublishedAt = publishedAt
+		post.PublishedAt = publishedAt
+		post.PublishedFriendly = publishedAt.Format("Jan 2, 2006")
 
-		if decodedFrontMatter.Permalink == "" {
+		if post.Permalink == "" {
 			// variant 1: 2011-12-31-23-59-59-hello-world.md
-			decodedFrontMatter.Permalink = dateTimeInFilename.ReplaceAllString(d.Name(), "")
+			post.Permalink = dateTimeInFilename.ReplaceAllString(d.Name(), "")
 
 			// variant 2: 2011-12-31-hello-world.md
-			decodedFrontMatter.Permalink = dateInFilename.ReplaceAllString(d.Name(), "")
+			post.Permalink = dateInFilename.ReplaceAllString(d.Name(), "")
 
 			// there's a leading hyphen, bridging the date and the title
-			decodedFrontMatter.Permalink = strings.TrimPrefix(decodedFrontMatter.Permalink, "-")
+			post.Permalink = strings.TrimPrefix(post.Permalink, "-")
 
-			decodedFrontMatter.Permalink = strings.TrimSuffix(decodedFrontMatter.Permalink, ".md")
+			post.Permalink = strings.TrimSuffix(post.Permalink, ".md")
 		} else {
 			// rather than formatting permalink as a "slug", it's actually a relative path to a directory
-			decodedFrontMatter.Permalink = strings.ReplaceAll(decodedFrontMatter.Permalink, "/", "")
+			post.Permalink = strings.ReplaceAll(post.Permalink, "/", "")
 		}
 
 		indexHtmlData.Posts = append(indexHtmlData.Posts, IndexPost{
-			Title:       decodedFrontMatter.Title,
-			Permalink:   decodedFrontMatter.Permalink,
-			PublishedAt: publishedAt.Format("Jan 2, 2006"),
-			timestamp:   int(publishedAt.Unix()),
+			Title:             post.Title,
+			Permalink:         post.Permalink,
+			PublishedFriendly: post.PublishedFriendly,
+			timestamp:         int(publishedAt.Unix()),
 		})
 
-		must(os.Mkdir(filepath.Join(docs, decodedFrontMatter.Permalink), 0775))
-		must(os.WriteFile(filepath.Join(docs, decodedFrontMatter.Permalink, "index.html"), outputHtml.Bytes(), 0664))
+		must(os.Mkdir(filepath.Join(docs, post.Permalink), 0775))
+
+		postIndexOutFile := must1(os.Create(filepath.Join(docs, post.Permalink, "index.html")))
+		must(postTemplate.Execute(postIndexOutFile, post))
 
 		return nil
 	})
@@ -137,18 +148,20 @@ type IndexHtmlData struct {
 	Posts []IndexPost
 }
 type IndexPost struct {
-	Title       string
-	Permalink   string
-	PublishedAt string
-	timestamp   int // for sorting reasons only
+	Title             string
+	Permalink         string
+	PublishedFriendly string
+	timestamp         int // for sorting reasons only
 }
 
-type PostMeta struct {
-	Title       string
-	PublishedAt time.Time
-	Date        string
-	Published   bool
-	Permalink   string
+type Post struct {
+	Content           template.HTML
+	Title             string
+	PublishedAt       time.Time
+	PublishedFriendly string
+	Date              string
+	Published         bool
+	Permalink         string
 }
 
 func must(err error) {
